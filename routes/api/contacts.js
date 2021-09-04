08b1c33652db45cbb832e31e90275e04ed89e448
Holovatiuk Victor <Stethem696@gmail.com>
2021-09-05 00:50:20 +0300

    Закончил домашку

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,85 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const contactsOperations = require("../../model");
-const { productSchema } = require("../../validation");
+const ctrl = require("../../controllers/contacts");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contacts = await contactsOperations.listContacts();
-    res.json({ contacts });
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/", ctrl.getAll);
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await contactsOperations.getContactById(contactId);
-    if (!contact) {
-      return res.status(404).json({
-        message: "Not found",
-      });
-    }
-    res.json({ contact });
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:contactId", ctrl.getById);
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = productSchema.validate(req.body);
-    if (error) {
-      return res.status(400).json({
-        message: error.message,
-      });
-    }
+router.post("/", ctrl.add);
 
-    const newContact = await contactsOperations.addContact(req.body);
-    res.status(201).json({ newContact });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:contactId", ctrl.delById);
 
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const deleteContact = await contactsOperations.removeContact(contactId);
-    if (!deleteContact) {
-      return res.status(404).json({
-        message: "Not found",
-      });
-    }
-    res.json({ deleteContact });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { error } = productSchema.validate(req.body);
-    if (error) {
-      return res.status(400).json({
-        message: error.message,
-      });
-    }
-
-    const { contactId } = req.params;
-    const contact = await contactsOperations.updateContact(contactId, req.body);
-    if (!contact) {
-      return res.status(404).json({
-        message: "Not found",
-      });
-    }
-
-    res.json({ contact });
-  } catch (error) {
-    next(error);
-  }
-});
+router.put("/:contactId", ctrl.updateById);
 
 module.exports = router;
