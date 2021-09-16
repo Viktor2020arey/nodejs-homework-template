7a9084bb06bdc8742b703562b44099711ee8a3f8
Holovatiuk Victor <Stethem696@gmail.com>
2021-09-16 08:18:39 +0300

    Закончил ДЗ

--- controllers/contacts/updateStatusContact.js
+++ controllers/contacts/updateStatusContact.js
@@ -10,14 +10,14 @@ const updateStatusContact = async (req, res, next) => {
       { new: true }
     );
     if (!contact) {
-      return res.status(400).json({
-        message: "missing field favorite",
+      return res.status(404).json({
+        message: "Not found",
       });
     }
 
     return contact
       ? res.status(200).json({ contact })
-      : res.status(400).json({ message: "not found" });
+      : res.status(404).json({ message: "Not found" });
   } catch (error) {
     next(error);
   }
